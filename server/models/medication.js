eadf8b3d9ff7f10993ca36327b9a85803939cd0f
mmk86 <marcin.klocek.86@gmail.com>
2016-03-05 16:43:58 +0100

    Changed the "Name" property to be used as ID.

--- server/models/medication.js
+++ server/models/medication.js
@@ -1,14 +1,12 @@
 module.exports = function(Medication) {
-  var isStatic = true;
-  Medication.disableRemoteMethod("createChangeStream", isStatic);
-  Medication.disableRemoteMethod("updateAll", isStatic);
-  Medication.disableRemoteMethod("find", isStatic);
-  Medication.disableRemoteMethod("findAll", isStatic);
-  Medication.disableRemoteMethod("upsert", isStatic);
-  Medication.disableRemoteMethod("updateAttributes", isStatic);
-  Medication.disableRemoteMethod("exists", isStatic);
-  Medication.disableRemoteMethod("count", isStatic);
-  Medication.disableRemoteMethod("findOne", isStatic);
-  Medication.disableRemoteMethod("deleteById", isStatic);
-
+  Medication.disableRemoteMethod("createChangeStream", true);
+  Medication.disableRemoteMethod("updateAll", true);
+  Medication.disableRemoteMethod("find", true);
+  Medication.disableRemoteMethod("findAll", true);
+  Medication.disableRemoteMethod("upsert", true);
+  Medication.disableRemoteMethod("updateAttributes", false);
+  Medication.disableRemoteMethod("exists", true);
+  Medication.disableRemoteMethod("count", true);
+  Medication.disableRemoteMethod("findOne", true);
+  Medication.disableRemoteMethod("deleteById", true);
 };
