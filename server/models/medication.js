ec60322b23a0d3aec63607718c939499f549814e
mmk86 <marcin.klocek.86@gmail.com>
2016-03-06 17:47:06 +0100

    Error handling and Firebase.
    Customized error message.
    Disabled stack trace on error.
    Added intergation with Firebase.

--- server/models/medication.js
+++ server/models/medication.js
@@ -1,4 +1,9 @@
+var Firebase = require("firebase");
+
 module.exports = function(Medication) {
+  "use strict";
+
+  const firebaseRef = new Firebase("https://the-medicine-task.firebaseio.com");
   Medication.disableRemoteMethod("createChangeStream", true);
   Medication.disableRemoteMethod("updateAll", true);
   Medication.disableRemoteMethod("find", true);
@@ -9,4 +14,23 @@ module.exports = function(Medication) {
   Medication.disableRemoteMethod("count", true);
   Medication.disableRemoteMethod("findOne", true);
   Medication.disableRemoteMethod("deleteById", true);
+
+  Medication.afterRemote("findById", function (ctx, medication, next) {
+    if (ctx.result) {
+      const medicationRef = firebaseRef.child(ctx.result.Name);
+      medicationRef.transaction(function (current) {
+        if (current === null) {
+          return 1;
+        }
+        return current + 1;
+      });
+      next();
+    } else {
+      const name = ctx.req.params.id;
+      var err = new Error(`The requested medication: "${name}" could not be found.`);
+      err.status = 404;
+      next(err);
+    }
+  });
+
 };
